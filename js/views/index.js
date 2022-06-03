7473aabcccb236c120c385641f512015fa3f1e83
HieuPham2000 <hieu.pt183535@gmail.com>
2022-06-03 10:53:01 +0700

    remove inline script

--- js/views/index.js
+++ js/views/index.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
   btnTry.on("click", () => {
     window.location = `${window.location.origin}/upload.html`;
   });
+
+  const container = document.querySelector('#container');
+  const panorama = new PANOLENS.ImagePanorama('./assets/unsplash1.jpg');
+  let viewer = new PANOLENS.Viewer({ container: container, autoRotate: true, autoRotateSpeed: 0.2, autoRotateActivationDuration: 3000 });
+  // viewer.addUpdateCallback(function () {
+
+  // });
+  viewer.add(panorama);
 })
